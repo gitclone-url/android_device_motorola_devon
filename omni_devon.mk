cd7c4abc0871ad25e9fbd4a082e9bdc25b9d6841
Abhijeet <98699436+gitclone-url@users.noreply.github.com>
2023-03-25 23:07:18 +0600

    Remove omni stuff!

--- omni_devon.mk
+++ omni_devon.mk
@@ -7,9 +7,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
